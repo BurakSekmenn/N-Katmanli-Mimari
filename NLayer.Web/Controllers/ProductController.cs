5db7be70ffe031cf9acde069434e9c3c3d0c0900
Burak Sekmen <burakskmn07@gmail.com>
2024-03-03 23:59:51 +0300

    Web

---
+++ NLayer.Web/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.Services;
+
+namespace NLayer.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductServices _services;
+
+        public ProductController(IProductServices services)
+        {
+            _services = services;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+
+            return Json(true);
+        }
+    }
+}
