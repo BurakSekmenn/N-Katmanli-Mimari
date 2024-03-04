9e6a2fb8ed1d873edebb9bac71bef6b608de562e
Burak Sekmen <burakskmn07@gmail.com>
2024-03-04 17:05:06 +0300

    Changed

---
+++ NLayer.WebNProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NLayer.WebNProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
